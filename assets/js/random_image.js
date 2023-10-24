281a38df5b208272aa329a368c983649a4dbe8bd
immerox <148655683+immerox@users.noreply.github.com>
2023-10-24 17:25:08 +0100

    Delete assets directory

--- assets/js/random_image.js
+++
@@ -1,7 +0,0 @@
-window.onload = displayImage;
-var imagesArray = ["Picsart_23-10-04_15-30-03-057.jpg"];
-
-function displayImage() {
-    var num = Math.floor(Math.random() * (imagesArray.length));
-    document.getElementById("main_image1").src = "assets/img/animes/" + imagesArray[num];
-}
